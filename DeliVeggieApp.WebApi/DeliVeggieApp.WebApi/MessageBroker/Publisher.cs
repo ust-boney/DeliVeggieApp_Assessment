1ff158b67404ee81f488824b0011b4f5c4a04621
boney84 <boney0084@gmail.com>
2025-05-02 09:36:23 +0530

    Added message broker in dotnet api

---
+++ DeliVeggieApp.WebApi/DeliVeggieApp.WebApi/MessageBroker/Publisher.cs
@@ -0,0 +1,40 @@
+﻿using RabbitMQ.Client;
+using System.Text;
+
+namespace DeliVeggieApp.WebApi.MessageBroker
+{
+    public class Publisher
+    {
+        private readonly string _hostname = "localhost";
+        private readonly string _queueName = "product-queue";
+        //private readonly string _username = "guest";
+        //private readonly string _password = "guest";
+
+        public void SendMessage(string message)
+        {
+            var factory = new ConnectionFactory()
+            {
+                HostName = _hostname
+            };
+            using var connection = factory.CreateConnection();
+            using var channel = connection.CreateModel();
+            channel.QueueDeclare(queue: _queueName,
+                             durable: true,
+                             exclusive: false,
+                             autoDelete: false,
+                             arguments: null);
+
+            var body = Encoding.UTF8.GetBytes(message);
+
+            var properties = channel.CreateBasicProperties();
+            properties.Persistent = true;
+
+            channel.BasicPublish(exchange: "",
+                                 routingKey: _queueName,
+                                 basicProperties: properties,
+                                 body: body);
+
+            Console.WriteLine($"[x] Sent: {message}");
+        }
+    }
+}

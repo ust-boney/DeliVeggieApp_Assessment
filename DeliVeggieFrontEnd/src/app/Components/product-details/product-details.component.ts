01723dab302f86d19f4764722f958aed91627793
boney84 <boney0084@gmail.com>
2025-05-04 13:36:47 +0530

    Modified frontend UI  product details and listing

--- DeliVeggieFrontEnd/src/app/Components/product-details/product-details.component.ts
+++ DeliVeggieFrontEnd/src/app/Components/product-details/product-details.component.ts
@@ -1,11 +1,11 @@
 import { Component, OnInit } from '@angular/core';
 import { ProductService } from '../../services/product.service';
-import { ActivatedRoute } from '@angular/router';
+import { ActivatedRoute, RouterLink } from '@angular/router';
 import { CurrencyPipe } from '@angular/common';
 
 @Component({
   selector: 'app-product-details',
-  imports: [CurrencyPipe],
+  imports: [CurrencyPipe,RouterLink],
   templateUrl: './product-details.component.html',
   styleUrl: './product-details.component.css'
 })
